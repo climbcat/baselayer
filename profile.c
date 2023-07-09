0fa12fd543426f6cdc30a0c3a0c5619fb73cffc7
climbcat <jakob.garde@gmail.com>
2023-07-09 16:15:52 +0200

    changing into a simpler, more compile-time, profiler

--- profile.c
+++ profile.c
@@ -1,7 +1,3 @@
-// TODO: make independent on arena
-// TODO: make independent on string
-// TODO: include gettimeofday currently in utils.c
-
 u64 ReadSystemTimerMySec() {
     u64 systime;
     struct timeval tm;
@@ -12,82 +8,69 @@ u64 ReadSystemTimerMySec() {
 }
 
 u64 ReadCPUTimer() {
-    // gcc:
-    u64 ticks = __builtin_ia32_rdtsc();
+    u64 ticks = __builtin_ia32_rdtsc(); // gcc
     return ticks;
 }
 
-struct ProfilerBlock {
-    ProfilerBlock *next = NULL;
-    String name;
-    u64 tsc_diff = 0;
-};
+// ***********
 
 struct Profiler {
-    MArena arena;
-    ProfilerBlock *first = NULL;
-    ProfilerBlock *current = NULL;
-
-    u64 cputime_diff = 0;
-    u64 systime_diff = 0;
-    float cpu_freq = 0;
+    u64 total_tsc;
+    u64 total_systime;
+    float cpu_freq;
+    u32 count;
 };
+struct ProfilerBlock {
+    u64 elapsed_tsc;
+    const char *tag;
+    u32 hits;
+};
+static Profiler g_prof;
+static ProfilerBlock g_prof_blocks[1024];
 
-Profiler ProfilerInit() {
-    ProfilerBlock block;
-    Profiler prof;
-    prof.arena = ArenaCreate();
-    prof.first = (ProfilerBlock*) ArenaPush(&prof.arena, &block, sizeof(block));
-    prof.current = prof.first;
-    prof.systime_diff = ReadSystemTimerMySec();
-    prof.cputime_diff = ReadCPUTimer();
-
-    return prof;
+void ProfilerStart(Profiler *p) {
+    p->total_systime = ReadSystemTimerMySec();
+    p->total_tsc = ReadCPUTimer();
 }
-
-ProfilerBlock *ProfilerReadBlockStart(Profiler *p, const char *func_name) {
-    ProfilerBlock *block = (ProfilerBlock*) ArenaPush(&p->arena, &block, sizeof(ProfilerBlock));
-    p->current = (ProfilerBlock*) InsertAfter1(p->current, block);
-    p->current->next = NULL;
-    p->current->name = StrLiteral(&p->arena, func_name);
-    p->current->tsc_diff = ReadCPUTimer();
-    return p->current;
-}
-void ProfilerReadBlockEnd(Profiler *p, ProfilerBlock *block) {
-    block->tsc_diff = ReadCPUTimer() - block->tsc_diff;
-}
-
 void ProfilerStop(Profiler *p) {
-    p->systime_diff = ReadSystemTimerMySec() - p->systime_diff;
-    p->cputime_diff = ReadCPUTimer() - p->cputime_diff;
-    p->cpu_freq = (float) p->cputime_diff / p->systime_diff;
-    p->current = NULL;
+    p->total_systime = ReadSystemTimerMySec() - p->total_systime;
+    p->total_tsc = ReadCPUTimer() - p->total_tsc;
+    p->cpu_freq = (float) p->total_tsc / p->total_systime;
 }
 void ProfilerPrint(Profiler *p) {
-    ProfilerBlock *current = p->first->next;
     printf("\n");
-    printf("Total time: %lu mysec", p->systime_diff);
-    printf(" (tsc: %lu,", p->cputime_diff);
+    printf("Total time: %lu mysec", p->total_systime);
+    printf(" (tsc: %lu,", p->total_tsc);
     printf(" freq [tsc/mys]: %f)\n", p->cpu_freq);
-    while (current != NULL) {
-        StrPrint("  %s: ", current->name);
-        printf("%lu (%f %%)\n", current->tsc_diff, (double) current->tsc_diff / p->cputime_diff * 100);
-        current = current->next;
+
+    ProfilerBlock *current;
+    for (int i = 1; i < g_prof.count + 1; ++i) {
+        current = g_prof_blocks + i;
+        printf("  %s: ", current->tag);
+        printf("%lu (%.2f%% %u hits)\n", current->elapsed_tsc, (double) current->elapsed_tsc / p->total_tsc * 100, current->hits);
     }
 }
 
-Profiler g_prof = ProfilerInit();
 class ProfileScopeMechanism {
     ProfilerBlock *block;
+    u64 start;
 public:
-    ProfileScopeMechanism(const char * func_name) {
-        this->block = ProfilerReadBlockStart(&g_prof, func_name);
+    ProfileScopeMechanism(const char *tag, u32 slot) {
+        this->start = ReadCPUTimer();
+
+        this->block = &g_prof_blocks[slot];
+        this->block->tag = tag;
+        this->block->hits += 1;
+        g_prof.count = MaxU32(g_prof.count, slot);
+
+
     }
     ~ProfileScopeMechanism() {
-        ProfilerReadBlockEnd(&g_prof, this->block);
+        this->block->elapsed_tsc += ReadCPUTimer() - this->start;
     }
 };
 
-#define TimeFunction ProfileScopeMechanism __prof_mechanism__(__FUNCTION__);
-#define TimeBlock(name) ProfileScopeMechanism __prof_mechanism__(name);
+#define TimeProgram ProfilerStart(&g_prof);
+#define TimeFunction ProfileScopeMechanism __prof_mechanism__(__FUNCTION__, __COUNTER__ + 1);
+#define TimeBlock(tag) ProfileScopeMechanism __prof_mechanism__("<" tag ">", __COUNTER__ + 1);
 #define TimePrint ProfilerStop(&g_prof); ProfilerPrint(&g_prof);
