df525a30387c85b1115320844160ad2d9eefb6a4
Jakob Garde <jakob.garde@gmail.com>
2023-09-13 00:13:31 +0200

    add platform.c file

---
+++ platform.c
@@ -0,0 +1,12 @@
+// platform-specific implementations of functions
+// not currently distributed in separate files
+
+#if defined __linux__ || defined __linux
+    #define LINUX 1
+    #define WINDOWS 0
+    #include "plaf/plaf_linux.c"
+#else 
+    #define LINUX 0
+    #define WINDOWS 1
+    #include "plaf/plaf_win.c"
+#endif
