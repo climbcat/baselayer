4da8bb800686ba833bf67a4249ba62c6cdd0c575
Jakob Garde <climbcat@users.noreply.github.com>
2023-12-11 12:19:15 +0100

    fix warning

--- utils.h
+++ utils.h
@@ -126,6 +126,10 @@ int RandMinMaxI(int min, int max) {
     assert(max > min);
     return McRandom() % (max - min + 1) + min;
 }
+f32 RandMinMaxI_f32(int min, int max) {
+    assert(max > min);
+    return (f32) (McRandom() % (max - min + 1) + min);
+}
 int RandDice(u32 max) {
     assert(max > 0);
     return McRandom() % max + 1;
