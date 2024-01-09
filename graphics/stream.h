14d15ca23ed98f854d592a3ef76c8aa9ec4aec6d
climbcat <jakob.garde@gmail.com>
2024-01-09 13:46:45 +0100

    code cleanup

--- graphics/stream.h
+++ graphics/stream.h
@@ -36,31 +36,28 @@ struct Vector2f {
     f32 y;
 };
 
-struct ImageF32 { // e.g. a depth image
+struct ImageF32 {
     u32 width;
     u32 height;
     f32 *data;
 };
-struct ImageU32 { // e.g. a bit map
+struct ImageU32 {
     u32 width;
     u32 height;
     f32 *data;
 };
 struct StreamHeader {
-    u32 next; // purpose: byte offset for iterating the chunk, zero indicates the final entry. (!Can in principle be != sizeof(EntityStream) + datasize)
-    StreamType tpe; // purpose: allows interpretation of data payload
-    u32 id; // purpose: for associating different EntityStream entries with the same object within the chunk (e.g. depth + colour)
-    u32 time; // purpose: real-time data header info for sorting & filtering by post- or external process
-    u32 datasize; // payload size
-    u32 linesize; // line size for 2d data
-    Matrix4f transform; // purpose: storage of this ever-present header info
+    u32 next; // byte offset to the next StreamHeader location
+    StreamType tpe;
+    u32 id; // streams of the same id belong to one enity
+    u32 time;
+    u32 datasize;
+    u32 linesize;
+    Matrix4f transform;
 
-    //
-    // utility functions
-
-    StreamHeader *GetNext(bool load_nonext = false) {
+    StreamHeader *GetNext(bool iter_nonext = false) {
         StreamHeader *result = NULL;
-        if (load_nonext && next == 0 && datasize > 0) {
+        if (iter_nonext && next == 0 && datasize > 0) {
             u32 calcnext = sizeof(StreamHeader) + datasize;
             result = (StreamHeader *) ((u8*) this + calcnext);
         }
@@ -69,20 +66,17 @@ struct StreamHeader {
         }
         return result;
     }
-    StreamHeader *GetNextId(bool load_nonext = false) {
-        StreamHeader *result = GetNext(load_nonext);
+    StreamHeader *GetNextId(bool iter_nonext = false) {
+        StreamHeader *result = GetNext(iter_nonext);
         while (result && result->id == this->id) {
-            result = result->GetNext(load_nonext);
+            result = result->GetNext(iter_nonext);
         }
         return result;
     }
     u8 *GetData() {
-        // returns a pointer to where the data is supposed to start (right after this struct's location in memory)
         return (u8*) this + sizeof(StreamHeader);
     }
     u32 GetDatumCount() {
-        // returns data count in iteration-ready units of 4B / 8B
-        // TODO: switch by type
         return datasize / sizeof(Vector3f);
     }
     List<Vector3f> GetDataVector3f() {
@@ -101,23 +95,11 @@ struct StreamHeader {
         return data;
     }
     u32 Get2DWidth() {
-        // returns data width (line width) in iteration-ready units of 4B / 8B
-        // TODO: switch by type
         return linesize / sizeof(Vector3f);
     }
     u32 GetDatumSize() {
-        // TODO: switch by type
         return sizeof(Vector3f);
     }
-    ImageU32 GetDataRGBA();
-    ImageF32 GetDataF32();
-    List<Vector2f> GetDataTexCoords();
-
-    // TODO: Q. How will EntityStream get ported to OGL? Using one VBO with DrawIndices, one VBO pr. stream, or something else?
-    //      Sbould data structure or EntityStream be redone to accomodate easy uploading of its data?
-    // NOTE: Entity is intended to become a mem-pooled scene graph node type, EntityStream an external, serializable
-    //      data storage system. Entity should be serializable as well, maybe as an EntityStream data type, but
-    //      first is must be pooled and id-based for its references, u16s that do next, parent, children
 };
 Vector3f _PointCloudAverage(Vector3f *data, u32 npoints, bool nonzero_only) {
     Vector3f result = Vector3f_Zero();
