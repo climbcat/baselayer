bc4281f845a7dc1dbacaf1f146f95ffdca5cd92a
Jakob Garde <jakob.garde@gmail.com>
2024-07-20 10:05:07 +0200

    a bit of sprite map code

--- graphics/atlasgen/main_spritegen.cpp
+++ graphics/atlasgen/main_spritegen.cpp
@@ -13,32 +13,33 @@
 #include "stb_image_write.h"
 
 
-void ExtractAliens() {
+struct Sprite {
+    Vector2i size;
+    Vector2f tex_0;
+    Vector2f tex_1;
+    u32 tex_id;
+};
 
-    MContext *ctx = InitBaselayer();
 
-    s32 bitmap_w;
-    s32 bitmap_h;
-    s32 channels_in_file;
-    u8 *data_01 = stbi_load("aliens_01.png", &bitmap_w, &bitmap_h, &channels_in_file, 3);
-    printf("%d %d %d\n", bitmap_w, bitmap_h, channels_in_file);
-
-    u8 *data_02 = stbi_load("aliens_02.png", &bitmap_w, &bitmap_h, &channels_in_file, 3);
-    printf("%d %d %d\n", bitmap_w, bitmap_h, channels_in_file);
-
+List<Sprite> CreateGridSprites(MArena *a_dest, u8* data, s32 bitmap_w, s32 bitmap_h, u32 texture_id) {
     // config vars
     s32 sprite_w2 = 10; // w2 == half width
     s32 sprite_h2 = 10; // h2 == half height
-
-    // auto vars
     s32 nx = 11;
     s32 ny = 6;
+
+    // auto vars
     s32 cell_w = bitmap_w / nx;
     s32 cell_h = bitmap_h / ny;
     f32 uw = sprite_w2 * 2.0f / bitmap_w;
     f32 vw = sprite_h2 * 2.0f / bitmap_h;
-    for (u32 i = 0; i < nx; ++i) {
-        for (u32 j = 0; j < ny; ++j) {
+
+    // return var
+    List<Sprite> sprites = InitList<Sprite>(a_dest, nx * ny);
+
+    // create sprites
+    for (u32 j = 0; j < ny; ++j) {
+        for (u32 i = 0; i < nx; ++i) {
             // cell center x, y
             s32 x = i * cell_w + cell_w / 2;
             s32 y = j * cell_h + cell_h / 2;
@@ -51,9 +52,72 @@ void ExtractAliens() {
             f32 u1 = u0 + uw;
             f32 v1 = v0 + vw;
 
-            // TODO: printf the coords for visual inspection
+            printf("alien %u: x: %d, y: %d, sleft: %d, stop: %d, u0: %.4f, u1: %.4f, v0: %.4f, v1: %.4f\n", j*nx + i, x, y, sleft, stop, u0, u1, v0, v1);
+
+            Sprite s = {};
+            s.tex_id = texture_id;
+            s.size = Vector2i { sprite_w2 * 2, sprite_h2 * 2 };
+            s.tex_0 = Vector2f { u0, v0 };
+            s.tex_1 = Vector2f { u1, v1 };
+
+            sprites.Add(s);
         }
     }
+
+    return sprites;
+}
+
+
+struct SpriteMap {
+    char map_name[32];
+    char key_name[32];
+    List<Sprite> sprites;
+    ImageRGBA texture;
+};
+SpriteMap CompileSpriteMapInline(MArena *a_dest, List<Sprite> sprites, HashMap *texture_map) {
+    SpriteMap smap;
+
+    // TODO: impl.
+
+    return smap;
+}
+
+
+void ExtractAliens() {
+
+    MContext *ctx = InitBaselayer();
+
+    s32 bitmap_w;
+    s32 bitmap_h;
+    s32 bitmap_w_02;
+    s32 bitmap_h_02;
+    s32 channels_in_file_01;
+    s32 channels_in_file_02;
+    u8 *data_01 = stbi_load("aliens_01.png", &bitmap_w, &bitmap_h, &channels_in_file_01, 3);
+    printf("%d %d %d\n", bitmap_w, bitmap_h, channels_in_file_01);
+    u8 *data_02 = stbi_load("aliens_02.png", &bitmap_w, &bitmap_h, &channels_in_file_02, 3);
+    printf("%d %d %d\n", bitmap_w_02, bitmap_h_02, channels_in_file_02);
+
+    assert(channels_in_file_01 == channels_in_file_02);
+    assert(bitmap_w == bitmap_w_02);
+    assert(bitmap_h == bitmap_h_02);
+
+    ImageRGBA tex_01 { bitmap_w, bitmap_h, (Color*) data_01};
+    ImageRGBA tex_02 { bitmap_w, bitmap_h, (Color*) data_02};
+
+    // organize the two bitmap textures by id
+    // TODO: get rid of the double de-referencing (first the map, then the *img) to access data
+    HashMap textures = InitMap(ctx->a_life, 2);
+    MapPut(&textures, 0, (u64) &tex_01);
+    MapPut(&textures, 1, (u64) &tex_02);
+
+    // lay out sprites
+    List<Sprite> sprites = CreateGridSprites(ctx->a_pers, data_01, bitmap_w, bitmap_h, 0);
+    CreateGridSprites(ctx->a_pers, data_01, bitmap_w, bitmap_h, 1);
+    sprites.len *= 2; // merge lists
+
+
+    SpriteMap smap = CompileSpriteMapInline(ctx->a_pers, sprites, &textures);
 
 
     stbi_write_bmp("spritemap.bmp", bitmap_w, bitmap_h, 3, data_02);
@@ -75,7 +139,7 @@ int main (int argc, char **argv) {
         Test();
     }
     else {
-        InitBaselayer();
+
         ExtractAliens();
     }
 }
