a6046355c2935305106a41191e4275f8a403a505
Jakob Garde <jakob.garde@gmail.com>
2025-03-05 10:07:59 +0100

    remove resource-load verbose logging

--- graphics/graphics.h
+++ graphics/graphics.h
@@ -42,13 +42,15 @@ GameLoopOne *InitGraphics(MContext *ctx, u32 width = 1280, u32 height = 800, con
     ResourceStreamHandle hdl = ResourceStreamLoadAndOpen(ctx->a_tmp, ctx->a_life, "all.res");
     g_font_names = hdl.names[RST_FONT];
 
+    bool log_verbose = false;
+
     // map out the resources
     ResourceHdr *res = hdl.first;
     while (res) {
         // fonts
         if (res->tpe == RST_FONT) {
             FontAtlas *font = FontAtlasLoadBinaryStream(res->GetInlinedData(), res->data_sz);
-            font->Print();
+            if (log_verbose) { font->Print(); }
 
             MapPut(&g_resource_map, font->GetKey(), font);
             MapPut(&g_texture_map, font->GetKey(), &font->texture);
@@ -57,7 +59,9 @@ GameLoopOne *InitGraphics(MContext *ctx, u32 width = 1280, u32 height = 800, con
         // sprite maps
         else if (res->tpe == RST_SPRITE) {
             SpriteMap *smap = SpriteMapLoadStream((u8*) res->GetInlinedData(), res->data_sz);
-            printf("sprite map: %s, %s, count: %u, atlas w: %u, atlas h: %u\n", smap->map_name, smap->key_name, smap->sprites.len, smap->texture.width, smap->texture.height);
+            if (log_verbose) {
+                printf("sprite map: %s, %s, count: %u, atlas w: %u, atlas h: %u\n", smap->map_name, smap->key_name, smap->sprites.len, smap->texture.width, smap->texture.height);
+            }
 
             MapPut(&g_resource_map, smap->GetKey(), smap);
             MapPut(&g_texture_map, smap->GetKey(), &smap->texture);
