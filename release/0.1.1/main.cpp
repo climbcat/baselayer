19b3463090b7f1a1e2a25fb8bc0e258d31bf0627
Jakob Garde <jakob.garde@gmail.com>
2025-07-18 08:14:15 +0200

    remove no-longer userful baselayer versions

--- release/0.1.1/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "jg_baselayer.h"
-
-
-int main (int argc, char **argv) {
-    printf("Compile test for baselayer 0.1.1\n");
-}
--- release/0.2.2/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "jg_baselayer.h"
-
-
-int main (int argc, char **argv) {
-    printf("Compile test for baselayer 0.2.2\n");
-
-    BaselayerAssertVersion(0, 2, 2);
-}
